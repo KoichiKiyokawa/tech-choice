a19a88daf3a419506078fd3088ce4e9b32b575c4
K.Kiyokawa <koichi20110068@gmail.com>
2021-05-02 12:08:04 +0900

    feat: query commit history by graphql

---
+++ src/modules/urql.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@urql/core";
+import fetch from "node-fetch";
+import { GITHUB_TOKEN } from "../env";
+
+export const urql = createClient({
+  url: "https://api.github.com/graphql",
+  // @ts-ignore
+  fetch,
+  fetchOptions: {
+    headers: {
+      authorization: `Bearer ${GITHUB_TOKEN}`, // なんの権限も付与していないトークンでOK
+    },
+  },
+});
--- src/scripts/gather-commit.ts
+++ src/scripts/gather-commit.ts
@@ -1,4 +1,10 @@
 import { PrismaClient } from "@prisma/client";
+import {
+  GetCommitHistory,
+  GetCommitHistoryQuery,
+  GetCommitHistoryQueryVariables,
+} from "../generated/graphql";
+import { urql } from "../modules/urql";
 
 const prisma = new PrismaClient();
 
@@ -9,6 +15,19 @@ const prisma = new PrismaClient();
 async function main() {
   // const frameworkId = process.argv[2];
   // if (frameworkId) console.log({ frameworkId });
+
+  const result = await urql
+    .query<GetCommitHistoryQuery, GetCommitHistoryQueryVariables>(
+      GetCommitHistory,
+      { name: "svelte", owner: "sveltejs", since: "2020-01-01T00:00:00+0000" }
+    )
+    .toPromise();
+  const target = result.data?.repository?.defaultBranchRef?.target;
+  if (target?.__typename === "Commit") {
+    target.history.nodes?.map((commit) => {
+      console.log(commit);
+    });
+  }
 }
 
 main()
