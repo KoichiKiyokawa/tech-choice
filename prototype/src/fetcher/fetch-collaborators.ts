de9ca2af3b36724480cbf0f2f3ad464ec58645e2
K.Kiyokawa <koichi20110068@gmail.com>
2021-06-26 11:02:56 +0900

    feat: pagenate collaborators query

--- prototype/src/fetcher/fetch-collaborators.ts
+++ prototype/src/fetcher/fetch-collaborators.ts
@@ -1,3 +1,4 @@
+import { last } from 'rhodash'
 import {
   GetCollaborators,
   GetCollaboratorsQuery,
@@ -11,14 +12,32 @@ import { urql } from '../modules/urql'
  * @returns コラボレータの名前が入った配列
  */
 export async function fetchCollaborators({ owner }: { owner: string }): Promise<string[]> {
-  const result = await urql
-    .query<GetCollaboratorsQuery, GetCollaboratorsQueryVariables>(GetCollaborators, {
-      owner,
-    })
-    .toPromise()
+  let collaboratorList: string[] = []
+  const cursor: { value?: string } = {}
 
-  return (
-    result.data?.organization?.membersWithRole?.edges?.flatMap((edge) => edge?.node?.login ?? []) ??
-    []
-  )
+  while (true) {
+    const result = await urql
+      .query<GetCollaboratorsQuery, GetCollaboratorsQueryVariables>(GetCollaborators, {
+        owner,
+        after: cursor.value,
+      })
+      .toPromise()
+
+    const edges = result.data?.organization?.membersWithRole.edges
+    if (edges == null) break
+
+    const collaborators = edges.flatMap((edge) => edge?.node?.login ?? [])
+
+    if (collaborators.length) collaboratorList.push(...collaborators)
+    else break // それ以上データを取得できなくなったら終了
+
+    cursor.value = last(edges)?.cursor
+  }
+
+  return collaboratorList
 }
+
+// fetchCollaborators({ owner: 'facebook' }).then((res) => {
+//   console.log(res)
+//   console.log(res.length)
+// })
