7dd063e121033e7204238e34317545d972e62217
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-13 17:35:27 +0900

    feat(dg): download repository

---
+++ data-gathering/src/repositories/framework.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client'
+import { FRAMEWORK_WITH_OWNER_LIST } from '../constants/framework-list'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.$connect()
+
+  await prisma.framework.createMany({
+    skipDuplicates: true,
+    data: FRAMEWORK_WITH_OWNER_LIST.map(({ name, owner }) => ({
+      name,
+      owner,
+    })),
+  })
+}
+
+main()
+  .catch(console.error)
+  .finally(() => prisma.$disconnect()) // .finally(prisma.$disconnect)とすると `Cannot read property '_clientVersion' of undefined`エラーがでる
