8fd187a4a74512861b8a5e64bd87defa8655f676
K.Kiyokawa <koichi20110068@gmail.com>
2021-07-30 12:22:37 +0900

    refactor: modify base fetch

--- web/src/utils/fetch.ts
+++ web/src/utils/fetch.ts
@@ -1,24 +1,25 @@
-import { BACKEND_ORIGIN } from '~/env.sample'
+import { BACKEND_ORIGIN } from '~/env'
 
-export async function baseFetch<Body = Record<string, unknown>, Response = void>(
+export async function baseFetch<Body = Record<string, unknown>, Resp = void>(
   endpoint: string,
   method: 'GET' | 'POST' = 'GET',
   data?: Body,
-): Promise<{ data: Response | null; error: Error | null; status: number }> {
-  const res = await fetch(`${BACKEND_ORIGIN}/${endpoint}`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method,
-    ...(data && { body: JSON.stringify(data) }),
-    credentials: 'include', // keep cookies for any domain
-  })
+): Promise<{ data: Resp | null; error: Error | null; status: number }> {
+  let res: Response
+  try {
+    res = await fetch(`${BACKEND_ORIGIN}/${endpoint}`, {
+      headers: {
+        ...(data && { 'Content-Type': 'application/json' }),
+      },
+      method,
+      ...(data && { body: JSON.stringify(data) }),
+      // credentials: 'include', // keep cookies for any domain
+    })
+  } catch (err) {
+    return { data: null, error: err, status: 500 }
+  }
 
   if (!res.ok) return { data: null, error: new Error(res.statusText), status: res.status }
 
-  try {
-    return { data: await res.json(), error: null, status: res.status }
-  } catch (err) {
-    return { data: null, error: err, status: -1 }
-  }
+  return { data: await res.json().catch(() => res.text), error: null, status: res.status }
 }
