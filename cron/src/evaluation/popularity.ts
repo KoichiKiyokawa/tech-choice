b6a1383db649285f0f72657b485b2bee8499e788
K.Kiyokawa <koichi20110068@gmail.com>
2021-09-17 15:23:49 +0900

    feat: ダウンロード数で差が出すぎないようにlogで緩やかにする

--- cron/src/evaluation/popularity.ts
+++ cron/src/evaluation/popularity.ts
@@ -3,6 +3,12 @@ import { Decimal } from 'decimal.js'
 import { fixedLastCalculatedAt } from '../constants/date'
 import { calcAgingScore } from '../utils/date'
 
+/**
+ * ダウンロード数は指数関数的に増えるため、人気度はその逆関数であるlogを使って算出する
+ * そのlogの底を表す定数
+ */
+const DOWNLOAD_BASE_OF_LOGARITHM = 1.3
+
 export function calcPopularityScoreForSpecificFramework({
   downloadHistories,
   starHistories,
@@ -15,9 +21,15 @@ export function calcPopularityScoreForSpecificFramework({
 }): Decimal {
   let result = new Decimal(0)
   for (const { count, downloadedAt } of downloadHistories) {
-    const eachDayScore = new Decimal(count).times(
+    // ダウンロード数は指数関数的に増えるため、人気度はその逆関数であるlogを使って算出する。
+    // logの底が第2引数にくることに注意 cf) https://mikemcl.github.io/decimal.js/#Dlog
+    // e.g. new Decimal(4).log(2) => log_2{4} = 2
+    const loosenedScore =
+      count >= 1 ? new Decimal(count).log(DOWNLOAD_BASE_OF_LOGARITHM) : new Decimal(0)
+    const eachDayScore = loosenedScore.times(
       calcAgingScore(dayjs(fixedLastCalculatedAt).diff(downloadedAt, 'day')),
     )
+
     result = result.plus(eachDayScore)
   }
 
